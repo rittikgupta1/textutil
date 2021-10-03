a718ee8cfb59ad0ecdf38022a9bc4653baaca5db
Ritik <rittikgupta1@gmail.com>
2021-10-03 14:54:57 +0530

    Exercise1

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -2,22 +2,38 @@ import { useState } from "react"
 import React from 'react'
 
 export default function Forms(props) {
-    const handleupClick= ()=>{
-        console.log("Capiyalze is clicked");
+    const handleupClick = () => {
         setText(text.toUpperCase());
     }
-    const Changehogya= (event)=>{
-        console.log("OnChange");
+    const Changehogya = (event) => {
         setText(event.target.value);
-        }
-    const[text,setText]=useState("Enter text here");
+    }
+    const handleupClicllower = () => {
+        setText(text.toLowerCase());
+    }
+    const handlespaces =()=>{
+        let newtext = text.trim();
+        setText(newtext);
+        };
+    const [text, setText] = useState("");
     return (
-        <div>
-            <h1>{props.heading}</h1>
-<div className="mb-3">
-  <textarea className="form-control" value={text} onChange={Changehogya} id="exampleFormControlTextarea1" rows="8"></textarea>
-</div>
-<button className="btn btn-primary" onClick={handleupClick}>Capitalize</button>
-</div>
+        <>
+            <div>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={Changehogya} id="exampleFormControlTextarea1" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleupClick}>Capitalize</button>
+                <button className="btn btn-primary" onClick={handleupClicllower}>LowerCase</button>
+                <button className="btn btn-primary mx-2" onClick={handlespaces}>Remove Extra spaces</button>
+     </div>
+           
+            <div className="container my-3">
+                <p>Numer of words:<strong>{text.split(" ").length}</strong>and  Number of characters:<strong>{text.length}</strong></p>
+                <p>Time taken to read: <strong> {text.split(" ").length*0.008}min</strong> </p>
+                <h2>preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
